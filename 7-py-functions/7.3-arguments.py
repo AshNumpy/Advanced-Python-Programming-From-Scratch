6bba13aaee8a493079f13c1cd6275b69f0c9f80e
Ash <ramazan.erduran@hacettepe.edu.tr>
2022-03-11 16:19:13 +0300

    7-py-functions

--- 7-py-functions/7.3-arguments.py
+++ 7-py-functions/7.3-arguments.py
@@ -29,3 +29,20 @@ n = sehirler[:]
 
 n[0] = 'İstanbul'
 print(sehirler)
+#***********************************
+
+def add(*params): # gönderilen argümanlar liste olarak alınır.
+    return sum(params)
+
+print(add(10,20))
+print(add(1,2,3,4,5,6))
+#***********************************
+
+def add2(*params):
+    sum=0
+    for i in params:
+        sum += i
+    return sum
+
+print(add2(1,2,3))
+#***********************************
